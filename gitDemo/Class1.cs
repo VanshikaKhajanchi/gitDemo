fba9f594929f0c4bf3ce581ab7b47bf49d87b342
Vanshika <vanshikaKhajanchi33@gmail.com>
2019-06-28 14:19:18 -0700

    one method added

--- gitDemo/Class1.cs
+++ gitDemo/Class1.cs
@@ -8,5 +8,12 @@ namespace gitDemo
 {
     public class Class1
     {
+        public int MyProperty { get; set; }
+        public int MyProperty2 { get; set; }
+        public void gitmethoddemo()
+        {
+            //some code
+        }
+
     }
 }
